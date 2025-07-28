30cc5684ee4b1ddad5b82e679cf6e85f8a672f4f
Cristian Coronel <cristian@xepelin.com>
2025-07-27 22:36:33 -0300

    chore(init.lua): Update mappings and comments in init.lua file

--- init.lua
+++ init.lua
@@ -4,21 +4,25 @@ local opts = { silent = true, noremap = true }
 vim.g.mapleader = " "
 map({ "n", "v" }, "<Space>", "<NOP>", opts)
 
+-- Escape mapping (ya configurado en settings.json)
 map("i", "kj", "<Esc>", opts)
+
+-- File operations
 map("n", "<leader>w", vim.cmd.w, opts)
 
--- move start and end of line
+-- Line navigation
 map("n", "gl", "$")
 map("n", "gh", "0")
 map("v", "gl", "$")
 map("v", "gh", "0")
 
--- move lines up and down
+-- Move lines up and down
 map("n", "<S-k>", ":m .-2<CR>==", opts)
 map("n", "<S-j>", ":m .+1<CR>==", opts)
 map("v", "<S-j>", ":m '>+1<CR>gv=gv", opts)
 map("v", "<S-k>", ":m '<-2<CR>gv=gv", opts)
 
+-- Split management
 map("n", "S", function()
 	vim.cmd('call VSCodeNotify("workbench.action.splitEditorDown")')
 end)
@@ -27,7 +31,7 @@ map("n", "s", function()
 	vim.cmd('call VSCodeNotify("workbench.action.splitEditorRight")')
 end)
 
--- Mover entre editores (como en tmux o Vim)
+-- Navigate between editors
 map("n", "<C-h>", function()
 	vim.cmd('call VSCodeNotify("workbench.action.navigateLeft")')
 end)
@@ -44,9 +48,10 @@ map("n", "<C-k>", function()
 	vim.cmd('call VSCodeNotify("workbench.action.navigateUp")')
 end)
 
-map("n", "<leader>f", function()
-	vim.cmd('call VSCodeNotify("workbench.action.quickOpen")')
-end)
+-- Quick actions
+-- map("n", "<leader>f", function()
+-- 	vim.cmd('call VSCodeNotify("workbench.action.quickOpen")')
+-- end)
 
 map("n", "<leader>q", function()
 	vim.cmd('call VSCodeNotify("workbench.action.closeActiveEditor")')
@@ -56,21 +61,26 @@ map("n", ";", function()
 	vim.cmd('call VSCodeNotify("editor.action.wordHighlight.next")')
 end, { silent = true })
 
+-- File explorer
 map("n", "<leader>e", function()
 	vim.cmd('call VSCodeNotify("vsnetrw.open")')
 end, { silent = true })
 
+-- Git
 map("n", "<leader>d", function()
 	vim.cmd('call VSCodeNotify("workbench.view.scm")')
 end)
 
+-- Go to implementation
 map("n", "gi", function()
 	vim.cmd('call VSCodeNotify("editor.action.goToImplementation")')
 end)
 
+-- Clipboard
 map("n", "yy", '"+yy')
 map("v", "y", '"+y')
 
+-- Git changes navigation
 map("n", "<cr>", function()
 	vim.cmd('call VSCodeNotify("workbench.action.editor.nextChange")')
 end)
