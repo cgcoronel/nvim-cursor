27956ae393fa77da499d0e7279a81489a76d7315
Cristian Coronel <cristian@xepelin.com>
2025-08-15 17:37:22 -0300

    chore(init.lua): consolidate line navigation options

--- init.lua
+++ init.lua
@@ -14,12 +14,11 @@ vim.g.mapleader = " "
 -- File operations
 map("n", "<leader>w", vim.cmd.w, opts)
 
--- Line navigation (más eficiente)
-local line_nav_opts = { noremap = true }
-map("n", "gl", "$", line_nav_opts)
-map("n", "gh", "0", line_nav_opts)
-map("v", "gl", "$", line_nav_opts)
-map("v", "gh", "0", line_nav_opts)
+-- Line navigation (consolidado)
+map("n", "gl", "$", opts)
+map("n", "gh", "0", opts)
+map("v", "gl", "$", opts)
+map("v", "gh", "0", opts)
 
 -- Move lines up and down
 map("n", "<S-k>", ":m .-2<CR>==", opts)
@@ -47,9 +46,9 @@ map("n", "<leader>e", vscode_command("vsnetrw.open"), opts)
 -- Go to implementation
 map("n", "gi", vscode_command("editor.action.goToImplementation"), opts)
 
--- Clipboard (sistema)
-map("n", "yy", '"+yy', { noremap = true })
-map("v", "y", '"+y', { noremap = true })
+-- Clipboard (consolidado)
+map("n", "yy", '"+yy', opts)
+map("v", "y", '"+y', opts)
 
 -- Git changes navigation
 map("n", "<cr>", vscode_command("workbench.action.editor.nextChange"), opts)
